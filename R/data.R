0ca7a88da12a8d11de7d045d2a86c41cd899b06b
Jonathan Bratt <jonathandanielbratt@gmail.com>
2021-07-30 12:31:57 -0500

    add sight words to data

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Sight Words
+#'
+#' A list of almost 1000 sight words.
+#'
+#' @format Character vector.
+#' @source \url{https://sightwords.com/sight-words/fry/#lists}
+"sight_words"
--- data-raw/sysdata.R
+++ data-raw/sysdata.R
@@ -23,3 +23,15 @@ rm(.prefix_name)
 rm(.suffix_name)
 rm(.interfix_name)
 rm(.lookup_url)
+
+# The sight word list was derived from the Fry sight words:
+# https://sightwords.com/sight-words/fry/#lists
+# Words with obvious breakdowns (e.g. "words" -> "word -s") were replaced with
+# the base word. A few cases of less-obvious breakdowns were similarly accepted
+# (e.g. "length" was excluded from the list to allow the breakdown "long -th").
+sight_words <- readLines("data-raw/sight_words.txt")
+usethis::use_data(
+  sight_words,
+  internal = FALSE, overwrite = TRUE
+)
+rm(sight_words)
