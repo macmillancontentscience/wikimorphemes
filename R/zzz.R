07d89d672240d41677df1b792986a53f4578e554
Jon Harmon <jonthegeek@gmail.com>
2021-08-17 13:47:21 -0500

    Update for better dlr. (#43)

--- R/zzz.R
+++ R/zzz.R
@@ -15,8 +15,7 @@
 # onload ------------------------------------------------------------------
 
 .onLoad <- function(libname, pkgname) {
-  set_wikimorphemes_cache_dir() # nocov start
-  .cache_wikitext <<- memoise::memoise(.cache_wikitext)
+  .cache_wikitext <<- memoise::memoise(.cache_wikitext) # nocov start
   .cache_lookup <<- memoise::memoise(.cache_lookup)
   .populate_env_lookup <<- memoise::memoise(.populate_env_lookup) # nocov end
 }
