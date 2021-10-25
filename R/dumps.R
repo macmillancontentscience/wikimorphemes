fdaf5c34d44439b1a678dc01ec560f4f2a3cb11e
Jon Harmon <jonthegeek@gmail.com>
2021-10-25 09:55:41 -0500

    Update data.world URLs. (#53)
    * Update data.world URLs.
    
    Also fixed a rare issue in dump processing.
    
    * Fixed comment.

--- R/dumps.R
+++ R/dumps.R
@@ -280,35 +280,35 @@
       # ns 0 is an actual article.
       if (ns == 0L) {
         # Attempt to just get the <text> block from this page to avoid weird
-        # situations. Error if the page doesn't have a text block, though.
+        # situations. If there isn't a text block, it looks like that means the
+        # page has been intentionally blanked out. I've seen one case of this
+        # ever but now we deal with it.
         this_wt <- stringr::str_extract(
           this_page,
           stringr::regex("<text.+</text>", dotall = TRUE)
         )
-        if (is.na(this_wt)) {
-          stop("No <text> at row ", i)
-        }
-
-        this_entry <- .extract_relevant_english_wt(this_wt)
-        if (length(this_entry)) {
-          title <- xml2::xml_text(
-            xml2::xml_find_first(
-              this_xml,
-              ".//title"
+        if (!is.na(this_wt)) {
+          this_entry <- .extract_relevant_english_wt(this_wt)
+          if (length(this_entry)) {
+            title <- xml2::xml_text(
+              xml2::xml_find_first(
+                this_xml,
+                ".//title"
+              )
             )
-          )
-          sha1 <- xml2::xml_text(
-            xml2::xml_find_first(
-              this_xml,
-              ".//sha1"
+            sha1 <- xml2::xml_text(
+              xml2::xml_find_first(
+                this_xml,
+                ".//sha1"
+              )
             )
-          )
-          word_info[[i]] <- data.frame(
-            row_n = i,
-            word = title,
-            wikitext = this_entry,
-            sha1 = sha1
-          )
+            word_info[[i]] <- data.frame(
+              row_n = i,
+              word = title,
+              wikitext = this_entry,
+              sha1 = sha1
+            )
+          }
         }
       }
     }
